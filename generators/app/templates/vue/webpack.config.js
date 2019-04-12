8e68d23c6f1c470222c8f41cc3cba96da1aec101
Sebastian Prein <hi@sebastianprein.com>
2019-04-12 13:39:28 +0200

    chore(vue): remove obsolete webpack options

--- generators/app/templates/vue/webpack.config.js
+++ generators/app/templates/vue/webpack.config.js
@@ -2,9 +2,4 @@ const { extendConfig } = require('@gridonic/webpack');
 
 module.exports = extendConfig
     .usePreset('vue')
-    .forAll({
-        html: {
-            template: './src/html/index.ejs'
-        }
-    })
     .toConfig;
