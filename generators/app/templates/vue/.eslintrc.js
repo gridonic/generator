fc58b3859f4b1bc44004bd8668d2e2730d4ceebd
jfierz <julien@gridonic.ch>
2019-12-09 19:19:44 +0100

    feat(vue-template): improve vue-config and ignore lines-between-members linting rule

--- generators/app/templates/vue/.eslintrc.js
+++ generators/app/templates/vue/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'class-methods-use-this': 'off',
+    'lines-between-class-members': 'off',
     'vue/script-indent': ['error', 2, {
       'baseIndent': 1,
       'ignores': []
